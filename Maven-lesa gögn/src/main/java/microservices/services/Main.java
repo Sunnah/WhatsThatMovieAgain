5a8d7a60ec19c7c370882fad366985397960a7f6
Sunna Halldórsdóttir <sunna@staki.is>
2017-01-16 08:42:06 +0000

    mvn, buin að na ollum single gognum

--- Maven-lesa gögn/src/main/java/microservices/services/Main.java
+++ Maven-lesa gögn/src/main/java/microservices/services/Main.java
@@ -57,42 +57,83 @@ public class Main {
     return 0;
   }
 
-  String getInfo(JSONObject json){
-    //try{
+  String getTitle(JSONObject json){
       if(json.get("title")!=null){
         return json.get("title").toString();  
       }
-      else return null; 
-   /* }
-    catch(ParseException e){
-      return "villa";
-    }*/
+      else return "null"; 
+  }
+
+  String getLanguage(JSONObject json){
+    if(json.get("original_language")!=null){
+      return json.get("original_language").toString();
+    }
+    else return "null";
+  }
+
+  String getRelease(JSONObject json){
+    if(json.get("release_date")!=null){
+      return json.get("release_date").toString();
+    }
+    else return "null";
+  }
+
+  int getLongest(String theme) throws Exception{
+    Main example = new Main();
+    int max=example.latest();
+    int longest=0;
+    for(int n=0;n<max;n++){
+      String response = example.getWebData(n);
+      try{
+        JSONObject json = (JSONObject)new JSONParser().parse(response);
+        if(json.get(theme)!=null){
+          String t = json.get(theme).toString();
+          int tala=t.split(",").length/2;
+          if(tala>longest){
+            longest=tala;
+            System.out.println(tala);
+          }
+        }
+      }
+      catch(ParseException e){}
+      /*if(n%5000==0){
+        System.out.println(n);
+      }*/
+    }
+    return longest;
   }
 
 
+
   public static void main(String[] args) throws Exception {
+    /*Main example = new Main();
+    int genres= example.getLongest("genres");
+    System.out.println("genres: "+genres);*/
     Main example = new Main();
     FileWriter fileWriter=null;
     try{
       fileWriter = new FileWriter("movieinf.csv");
+      fileWriter.append("Title;Original_language;Release_date");
       int max= example.latest();
-      for(int n=0; n<=max; n++){
+      for(int n=0; n<=20; n++){
         String response = example.getWebData(n);
         if(response.split(",")[0].matches(".*\"status_code\":25")){
           System.out.println("þyrfti að gera ehv hér");
           response=example.getWebData(n);
         }
-        String s=null;
         if(!response.split(",")[0].matches(".*\"status_code\":34")){
           try{
             JSONObject json = (JSONObject)new JSONParser().parse(response);
-            s = example.getInfo(json);
+            fileWriter.append("\n");
+            fileWriter.append(example.getTitle(json));
+            fileWriter.append(";");
+            fileWriter.append(example.getLanguage(json));
+            fileWriter.append(";");
+            fileWriter.append(example.getRelease(json));
           }
           catch(ParseException e){
-            s="villa";
+            
           }
-          fileWriter.append("\n");
-          fileWriter.append(s);
         }
       }
     }
